b8ea0ad737ddccc718777bfecae97c9753563ec3
unknown <ashwathshetty43@gmail.com>
2021-05-18 21:02:16 +0530

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl,FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm = this.fb.group ({
+    name:[''],
+    password:['']
+  })
+  constructor(private fb:FormBuilder,private route:Router) { }
 
   ngOnInit() {
   }
-
+  onSubmit(){
+    console.log("login",this.loginForm.value);
+    this.route.navigate(['/eventDetails']);
+  }
 }
