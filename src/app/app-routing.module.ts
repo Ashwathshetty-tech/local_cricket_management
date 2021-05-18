0e69cbe65d1f9fcb61b648a872de0590c471e39f
unknown <ashwathshetty43@gmail.com>
2021-05-18 19:17:19 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { EventRegisterComponent } from './Events/event-register/event-register.component';
+import { EventDetailsComponent } from './Events/event-details/event-details.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'eventDetails',component:EventDetailsComponent},
+  {path:'eventRegister',component:EventRegisterComponent},
+   { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EventRegisterComponent } from './Events/event-register/event-register.component';
+import { EventDetailsComponent } from './Events/event-details/event-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EventRegisterComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
